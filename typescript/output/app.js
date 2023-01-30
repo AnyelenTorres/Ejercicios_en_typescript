2b13bc49c5f2fc58f4f1f5de37666c71ec3a8558
unknown <giselanyelentorres@gmail.com>
2023-01-29 21:23:35 -0300

    ejercicios de argentina programa

---
+++ typescript/output/app.js
@@ -0,0 +1,16 @@
+"use strict";
+let message = "hello world";
+console.log(message);
+
+console.log(3 + true);
+var i =1;
+var j = i;
+if(i ==1){
+    var i =2
+}
+function x(){
+    i=3;
+    i+=3;
+}
+x();
+console.log(i);
---
+++ typescript/prueba2.ts
@@ -0,0 +1,15 @@
+//tipo de datos
+//string
+let saludo:string="hola mundo";
+let colorProducto ="blue";
+//number
+let codigoProducto:number =6;
+//boolean
+let nombreProducto:boolean=false;
+///arrays
+let ñista:number[]=[1,2,3];
+let lista: string[]=["pimiento","cebolla","tomate"];
+//any
+let cantidad:any =4;
+let desc: any[1,true,violeta]
+
